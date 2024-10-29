04bf42df5e404c02898ef9bb16047648a549f36e
minseokcha@laptop <alstjr0526@gmail.com>
2024-10-29 16:23:07 +0900

    복구

--- 2DGP project -night_of_knight-.py
+++ 2DGP project -night_of_knight-.py
@@ -1,5 +1,6 @@
 from pico2d import *
 
+
 WIDTH, HEIGHT = 960, 800
 move_x, move_y = 0, 0
 direction = 1           # 1==오른쪽, 0 == 왼쪽
@@ -72,13 +73,14 @@ def handle_events():
             elif event.key == SDLK_d:  # 방어
                 is_protecting = False
 
-class Back_ground_swamp:
+class Background_swamp:
     global move_x
     def __init__(self):
 
         self.x = WIDTH
         self.Back_ground_swamp = load_image('bg_tile_chapter_02_02x2.png')
         self.scroll_speed = 2
+        pass
     def update(self):
         global background_move
         if self.x + self.scroll_speed * move_x < WIDTH and self.x + self.scroll_speed * move_x > -1920 + WIDTH*2 and background_move:
@@ -90,14 +92,16 @@ class Back_ground_swamp:
         pass
 
     def draw(self):
-        self.Back_ground_swamp.draw(self.x, HEIGHT // 2)
-class Back_ground_lava:
+            self.Back_ground_swamp.draw(self.x, HEIGHT // 2)
+
+class Background_lava:
     global move_x
     def __init__(self):
 
         self.x = WIDTH
         self.Back_ground_lava = load_image('bg_tile_chapter_02_03x2.png')
         self.scroll_speed = 2
+        pass
     def update(self):
         global background_move
         if self.x + self.scroll_speed * move_x < WIDTH and self.x + self.scroll_speed * move_x > -1920 + WIDTH*2 and background_move:
@@ -109,15 +113,33 @@ class Back_ground_lava:
         pass
 
     def draw(self):
-        self.Back_ground_lava.draw(self.x, HEIGHT // 2)
+            self.Back_ground_lava.draw(self.x, HEIGHT // 2)
 
-class Tile:
+class Tile_ground_swamp:
     global move_x
     i =0
-
     def __init__(self):
         self.x = 0
         self.tile_swamp = load_image('tile_chapter_0000_tile1_.png')
+        self.scroll_speed = 5
+
+    def update(self):
+        global background_move
+        if background_move:
+            self.x += self.scroll_speed * move_x
+
+    def handle_event(self):
+        pass
+
+    def draw(self):
+        for i in range(37):
+                self.tile_swamp.draw(self.x + 128 * i, 50,192,136)
+
+class Tile_ground_lava:
+    global move_x
+    i =0
+    def __init__(self):
+        self.x = 0
         self.tile_lava = load_image('tile_chapter_0004_tile2.png')
         self.scroll_speed = 5
 
@@ -131,20 +153,16 @@ class Tile:
 
     def draw(self):
         for i in range(37):
-            if stage == 1:
-                self.tile_swamp.draw(self.x + 128 * i, 50,192,136)
-            elif stage == 2:
                 self.tile_lava.draw(self.x + 128 * i, 50, 192, 136)
-
-
 
 
 class Knight:
     global is_running
     global direction
+
     def __init__(self):
-        self.x, self.y = WIDTH//2, 168
-        self.y_foot = self.y -35
+        self.x, self.y = WIDTH // 2, 168
+        self.y_foot = self.y - 35
         self.hp_max, self.stamina_max, self.power = 1000, 100, 100
         self.hp_now, self.stamina_now = 1000, 100
         self.frame_Idle, self.frame_Idle_timer = 0, 0
@@ -161,39 +179,37 @@ class Knight:
         self.image_Attack3 = load_image('Knight_Attack 3.png')
         self.image_Protect = load_image('Knight_Protect.png')
 
-
     def update(self):
         global background_move
         global stage
-        global move_y, is_jumping, is_attacking, is_protecting, attack_count
+        global move_x, move_y, is_jumping, is_attacking, is_protecting, attack_count
 
-        self.y += move_y        #기사는 중력(move_y)에 의해 항상 y값이 줄어든다.
-        move_y -=1
 
-        if self.y == 168:       # 기사는 '타일을 밟고 있을 땐' y값이 줄어들지 않고, 점프를 하고 있는게 아니다.
-            move_y = 0          # 타일을 밟다 == self.x와 self.y_foot의 값이 tile_top과 같다...? tile_top은 x,y좌표료 이루어져 있음
+        self.y += move_y  # 기사는 중력(move_y)에 의해 항상 y값이 줄어든다.
+        move_y -= 1
+
+        if self.y == 168:  # 기사는 '타일을 밟고 있을 땐' y값이 줄어들지 않고, 점프를 하고 있는게 아니다.
+            move_y = 0  # 타일을 밟다 == self.x와 self.y_foot의 값이 tile_top과 같다...? tile_top은 x,y좌표료 이루어져 있음
             is_jumping = False
 
         if self.stamina_now < self.stamina_max:
-            self.stamina_now += 0.1         #1초에 10씩 스테미나 회복
-
+            self.stamina_now += 0.1  # 1초에 10씩 스테미나 회복
 
         if not background_move:
             if stage == 1:
                 if self.x - 5 * move_x > 10:
                     self.x -= 5 * move_x
             elif stage == 2:
-                if  10 < self.x - 5 * move_x < 950:
+                if 10 < self.x - 5 * move_x < 950:
                     self.x -= 5 * move_x
 
-        if self.x == WIDTH//2:
+        if self.x == WIDTH // 2:
             background_move = True
-        if self.x > WIDTH:
+
+        if self.x > WIDTH:  # knight가 화면 밖으로 나가면 2스테이지 2로 넘어감
             stage = 2
             self.x = WIDTH // 2
             background_move = True
-
-
 
         if is_running:
             if self.frame_Run_timer >= 8:  # run 애니메이션
@@ -201,21 +217,15 @@ class Knight:
                 self.frame_Run_timer = 0
             else:
                 self.frame_Run_timer += 1
-        elif is_jumping:
-            if self.frame_Jump_timer >= 15:  # jump 애니메이션
-                self.frame_Jump = (self.frame_Jump + 1) % 6
-                self.frame_Jump_timer = 0
-            else:
-                self.frame_Jump_timer += 1
         elif is_attacking:
             if self.frame_Attack_timer >= 6:  # attack 애니메이션
-                self.frame_Attack = (self.frame_Attack + 1) % 5     #0.06초에 1프레임, 총 4프레임 있음
+                self.frame_Attack = (self.frame_Attack + 1) % 5  # 0.06초에 1프레임, 총 4프레임 있음
                 self.frame_Attack_timer = 0
             else:
                 self.frame_Attack_timer += 1
 
-            if self.attack_motion <= attack_count and self.frame_Attack == 4:   #4프레임이 다 그려졌을 때 다음 모션, 프레임 초기화
-                self.attack_motion +=1
+            if self.attack_motion <= attack_count and self.frame_Attack == 4:  # 4프레임이 다 그려졌을 때 다음 모션, 프레임 초기화
+                self.attack_motion += 1
                 self.frame_Attack = 0
             elif self.attack_motion > attack_count:
                 is_attacking = False
@@ -223,11 +233,18 @@ class Knight:
                 self.frame_Attack = 0
                 attack_count = 0
 
+        elif is_jumping:
+            if (self.frame_Jump_timer >= 15):  # jump 애니메이션
+                self.frame_Jump = (self.frame_Jump + 1) % 6
+                self.frame_Jump_timer = 0
+            else:
+                self.frame_Jump_timer += 1
+
         elif is_protecting:
-            self.stamina_now -= 0.5     #방어중이면 스테미나 감소
+            self.stamina_now -= 0.5  # 방어중이면 스테미나 감소
             print(self.stamina_now)
 
-            if self.stamina_now <0:
+            if self.stamina_now < 0:
                 is_protecting = False
 
 
@@ -242,9 +259,9 @@ class Knight:
         pass
 
     def draw(self):
-        if is_running and self.y == 168 :
+        if is_running and self.y == 168:
             if direction:
-                self.image_Run.clip_draw(self.frame_Run * 128, 0, 70, 70, self.x, self.y,105,105)
+                self.image_Run.clip_draw(self.frame_Run * 128, 0, 70, 70, self.x, self.y, 105, 105)
             else:
                 self.image_Run.clip_composite_draw(self.frame_Run * 128, 0, 70, 70, 0, 'h', self.x, self.y, 105, 105)
 
@@ -259,62 +276,69 @@ class Knight:
                 pass
             else:
                 if self.attack_motion == 1:
-                    self.image_Attack1.clip_composite_draw(self.frame_Attack * 128, 0, 100, 70, 0, 'h', self.x - 10, self.y, 150, 105)
+                    self.image_Attack1.clip_composite_draw(self.frame_Attack * 128, 0, 100, 70, 0, 'h', self.x - 10,
+                                                           self.y, 150, 105)
                 if self.attack_motion == 2:
-                    self.image_Attack2.clip_composite_draw(self.frame_Attack * 128, 0, 120, 70, 0, 'h', self.x - 20,self.y, 180, 105)
+                    self.image_Attack2.clip_composite_draw(self.frame_Attack * 128, 0, 120, 70, 0, 'h', self.x - 20,
+                                                           self.y, 180, 105)
                 if self.attack_motion == 3:
-                    self.image_Attack3.clip_composite_draw(self.frame_Attack * 128, 0, 100, 70, 0, 'h', self.x - 10,self.y, 150, 105)
+                    self.image_Attack3.clip_composite_draw(self.frame_Attack * 128, 0, 100, 70, 0, 'h', self.x - 10,
+                                                           self.y, 150, 105)
 
         elif is_jumping:
             if direction:
-                self.image_Jump.clip_draw(self.frame_Jump * 128, 0, 70, 70, self.x, self.y,105,105)
+                self.image_Jump.clip_draw(self.frame_Jump * 128, 0, 70, 70, self.x, self.y, 105, 105)
             else:
                 self.image_Jump.clip_composite_draw(self.frame_Jump * 128, 0, 70, 70, 0, 'h', self.x, self.y, 105, 105)
 
         elif is_protecting:
             if direction:
-                self.image_Protect.clip_draw(0, 0, 70, 70, self.x, self.y,105,105)
+                self.image_Protect.clip_draw(0, 0, 70, 70, self.x, self.y, 105, 105)
             else:
                 self.image_Protect.clip_composite_draw(0, 0, 70, 70, 0, 'h', self.x, self.y, 105, 105)
 
-        else: #idle
+        else:  # idle
             if direction:
-              self.image_Idle.clip_draw(self.frame_Idle * 128, 0, 55, 70, self.x, self.y,83,105)
+                self.image_Idle.clip_draw(self.frame_Idle * 128, 0, 55, 70, self.x, self.y, 83, 105)
             else:
-                self.image_Idle.clip_composite_draw(self.frame_Idle * 128, 0, 55, 70, 0, 'h', self.x, self.y,83,105)
+                self.image_Idle.clip_composite_draw(self.frame_Idle * 128, 0, 55, 70, 0, 'h', self.x, self.y, 83, 105)
 
 def reset_world():
     global alive
     global world
     global knight
-    global bg_swamp, tile_swamp
-    global bg_lava, tile
+    global background_swamp, tile_ground_swamp
+    global background_lava, tile_ground_lava
+
     global stage
 
     alive = True
     world = []
 
-    bg_swamp = Back_ground_swamp()
-    tile = Tile()
+    background_swamp = Background_swamp()
+    tile_ground_swamp = Tile_ground_swamp()
 
-    bg_lava = Back_ground_lava()
+    background_lava = Background_lava()
+    tile_ground_lava = Tile_ground_lava()
+
 
     knight = Knight()
 
-    world.append(knight)    #기사를 월드에 추가
-    world.append(tile)
 
+    world.append(knight)        # 월드에 기사를 추가
 
 def update_world():
     global stage
-    if stage ==1:
-        bg_swamp.update()   # 늪배경 업데이트
+    if stage == 1:
+        background_swamp.update()         # 배경 업데이트
+        tile_ground_swamp.update()  # 타일 업데이트
+
     if stage == 2:
-        bg_lava.update()    # 용암배경 업데이트
+        background_lava.update()        # 배경 업데이트
+        tile_ground_lava.update()  # 타일 업데이트
 
+    knight.update()         # 기사 업데이트
 
-    tile.update()
-    knight.update()  # 기사 업데이트
     #몬스터 업데이트
     #보스 업데이트
     #엘릭서 업데이트
@@ -324,13 +348,18 @@ def update_world():
 def render_world():
     global stage
     clear_canvas()
+
     if stage == 1:
-        bg_swamp.draw()
-    elif stage == 2:
-        bg_lava.draw()
+        background_swamp.draw()  # 배경 업데이트
+        tile_ground_swamp.draw()  # 타일 업데이트
+
+    if stage == 2:
+        background_lava.draw()  # 배경 업데이트
+        tile_ground_lava.draw()  # 타일 업데이트
+
 
     for o in world:
-        o.draw()
+        o.draw()        #월드 출력
 
     update_canvas()
 
